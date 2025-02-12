df1cfb68a9d936f93c83972d251987b8e10498ae
RedzCode88 <vicente00tiago@gmail.com>
2025-02-12 19:05:40 +0000

    todosComvariosExemplos

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,111 @@
-// App.jsx
-import './App.css';
-import Laranjas from './components/Laranjas';
-import BoxColor from './components/BoxColor';
-import CreditCard from './components/CreditCard';
 import React from 'react';
+import './App.css';
+import { useState } from 'react';
+import todosJSON from './assets/todos.json'
+import Todo from './components/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <CreditCard
-        type="Visa"
-        number="0123456789018845"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="BNP"
-        owner="Maxence Bouret"
-        bgColor="#11aa99"
-        color="white"
-      />
 
-      <CreditCard
-        type="Master Card"
-        number="0123456789010995"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="N26"
-        owner="Maxence Bouret"
-        bgColor="#eeeeee"
-        color="#222222"
-      />
+  // Inicializar Variáveis
+  const [todos, setTodos] = useState(todosJSON)
+  const [todoTitle, setTodoTitle] = useState("")
 
-      <CreditCard
-        type="Visa"
-        number="0123456789016984"
-        expirationMonth={12}
-        expirationYear={2019}
-        bank="Name of the Bank"
-        owner="Firstname Lastname"
-        bgColor="#ddbb55"
-        color="white"
-      />
-    </div>
-  );
+  // Código para gerar novo ID com base no ID mais alto que já existe (somado de 1)
+  const newId = () => {
+    let highestTodo = 0
+
+    todos.forEach(todo => {
+      if (todo.id > highestTodo) {
+        highestTodo = todo.id
+      }
+    })
+
+    return highestTodo + 1
+  }
+
+  // Ao alterar o input, atualizar a variável state
+  const handleChange = (e) => {
+    setTodoTitle(e.target.value)
+  }
+
+  // Toggle Todo - Ao clicar num todo, trocar o "completed"
+  const toggleTodo = (id) => {
+
+    // 1. Criar uma cópia da nossa variável
+    const todosCopy = [...todos]
+
+    // 2. Encontrar o todo com o id selecionado (na cópia)
+    todosCopy.forEach(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+    })
+
+    // 3. Atualizar a variável "todos" com a cópia *alterada*
+    setTodos(todosCopy)
+  }
+
+  const editTodoTitle = (id, title) => {
+
+    // 1. Criar uma cópia da nossa variável
+    const todosCopy = [...todos]
+
+    // 2. Encontrar o todo com o id selecionado (na cópia)
+    todosCopy.forEach(todo => {
+      if (todo.id === id) {
+        todo.title = title
+      }
+    })
+
+    // 3. Atualizar a variável "todos" com a cópia *alterada*
+    setTodos(todosCopy)
+  }
+
+  const deleteTodo = (id) => {
+    const todosCopy = todos.filter(todo => todo.id !== id)
+    setTodos(todosCopy)
+  }
+
+  // Novo Todo - Ao clicar no botão de submissão, colocar um novo Todo com info do utilizador
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    if (!todoTitle) return
+
+    const novoTodo = {
+      id: newId(),
+      title: todoTitle,
+      completed: false
+    }
+
+    // Resetar o input após submissão
+    setTodoTitle("")
+
+    // Criar cópia do array com um novo todo e seta-o
+    setTodos([...todos, novoTodo])
+  }
+
+  const todosFeitos = () => todos.filter(todo => todo.completed).length
+
+  return (<div className='App'>
+
+    {/* Colocar a lista de Todos */}
+    {todos.map(todo => <Todo key={todo.id} todo={todo}
+      editTodoTitle={editTodoTitle}
+      deleteTodo={deleteTodo}
+      toggleTodo={toggleTodo}
+    />)}
+
+    {/* Formulário de Submissão de um novo Todo */}
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} value={todoTitle} />
+      <button>Adicionar Todo</button>
+    </form>
+
+    Todos Feitos: {todosFeitos()}
+
+  </div>)
 }
 
 export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todo(props) {
 
-    const {todo} = props
-    const {title, completed} = todo
+    const { todo, toggleTodo, deleteTodo, editTodoTitle } = props
 
-    const completedClass = completed ? "done" : "undone"
+    const completedClass = todo.completed ? "completed" : "incomplete"
 
-  return (
-    <div className={'todo' + completedClass}>
-        <div className='title'>{title}</div>
-    </div>
-  )
+    const [isEditing, setIsEditing] = useState(false)
+    const [title, setTitle] = useState(todo.title)
+
+    const handleChange = e => setTitle(e.target.value)
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        editTodoTitle(todo.id, title)
+        setIsEditing(false)
+    }
+
+    return (
+        <div className={`todo ` + completedClass}>
+
+            {!isEditing
+                ? <div>
+
+                    <h3>{title}</h3>
+
+                    <div className='status'>
+                        {todo.completed ? <div>Completed</div> : <div>Incomplete</div>}
+                    </div>
+
+                    <button onClick={() => deleteTodo(todo.id)}>
+                        Eliminar
+                    </button>
+
+                    <button onClick={() => toggleTodo(todo.id)}>
+                        Toggle
+                    </button>
+
+                    <button onClick={() => setIsEditing(true)}>
+                        Edit
+                    </button>
+
+                </div>
+                : <form onSubmit={handleSubmit}>
+                    <input type="text" value={title} onChange={handleChange} />
+                    <button>Save Changes</button>
+                </form>
+            }
+
+        </div>
+    )
 }
 
 export default Todo
